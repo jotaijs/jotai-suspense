de114f846dc4e67efb885b235896f11d86a8f35c
Anton Petrov <apetrov@ourgapps.com>
2022-05-18 15:34:15 +0300

    Add example

---
+++ examples/03_pending/src/App.tsx
@@ -0,0 +1,45 @@
+import React, { Suspense } from 'react';
+import { Provider, useSetAtom, useAtomValue } from 'jotai';
+import { atomWithPending } from 'jotai-suspense';
+
+const workAtom = atomWithPending<string>();
+
+const WorkController = () => {
+  const done = useSetAtom(workAtom);
+  return (
+    <>
+      <button type="button" onClick={() => done('success')}>
+        success
+      </button>
+      <button type="button" onClick={() => done('fail')}>
+        fail
+      </button>
+    </>
+  );
+};
+
+const WorkStatus = () => {
+  const status = useAtomValue(workAtom);
+  return <>Work is done with {status}.</>;
+};
+
+const Work = () => {
+  return (
+    <>
+      <WorkController />
+      <Suspense fallback="Work in progress...">
+        <WorkStatus />
+      </Suspense>
+    </>
+  );
+};
+
+const App = () => {
+  return (
+    <Provider>
+      <Work />
+    </Provider>
+  );
+};
+
+export default App;
